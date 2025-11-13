9bf9bfa7025adb38aa71c6cf4e09552fbbc71934
yangkeyin <2364483038@qq.com>
2025-11-13 17:55:26 +0800

    fix:解决了负号的问题

--- new_scripts/run_transformer_freq_limit.py
+++ new_scripts/run_transformer_freq_limit.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import os
 
 # 设置Matplotlib支持中文
-plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
+plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']
 plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
 
 # --- 配置 ---
